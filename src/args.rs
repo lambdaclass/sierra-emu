c4d1e4770b780a276eb243a5d35b1c7a488b2ae0
Esteve Soler Arderiu <soler.arderiu@gmail.com>
2024-07-31 16:12:45 +0200

    Add CLI and tracing.

---
+++ src/args.rs
@@ -0,0 +1,25 @@
+use clap::Parser;
+use std::{num::ParseIntError, path::PathBuf, str::FromStr};
+
+#[derive(Debug, Parser)]
+pub struct CmdArgs {
+    pub program: PathBuf,
+    pub entry_point: EntryPoint,
+}
+
+#[derive(Clone, Debug)]
+pub enum EntryPoint {
+    Number(usize),
+    String(String),
+}
+
+impl FromStr for EntryPoint {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s.chars().next() {
+            Some(x) if x.is_numeric() => Self::Number(s.parse()?),
+            _ => Self::String(s.to_string()),
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use self::args::CmdArgs;
+use clap::Parser;
+use tracing::{info, Level};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+mod args;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = CmdArgs::parse();
+
+    tracing::subscriber::set_global_default(
+        FmtSubscriber::builder()
+            .with_env_filter(EnvFilter::from_default_env())
+            .with_max_level(Level::TRACE)
+            .finish(),
+    )?;
+
+    info!("Hello, world!");
+    info!("Args: {args:?}");
+
+    Ok(())
 }
