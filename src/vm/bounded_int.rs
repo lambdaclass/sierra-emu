11803e200920de291f54f05ff6be58b5c2ec2a9b
Edgar Luque <git@edgarluque.com>
2025-01-20 14:31:09 +0100

    fix tests

--- src/vm/bounded_int.rs
+++ src/vm/bounded_int.rs
@@ -327,7 +327,7 @@ mod tests {
             use core::internal::{OptionRev, bounded_int::BoundedInt};
             use core::internal::bounded_int;
             fn main() -> BoundedInt<-127, 127> {
-                let num = match bounded_int::trim::<i8, -0x80>(1) {
+                let num = match bounded_int::trim_min::<i8>(1) {
                     OptionRev::Some(n) => n,
                     OptionRev::None => 1,
                 };
@@ -352,7 +352,7 @@ mod tests {
             use core::internal::{OptionRev, bounded_int::BoundedInt};
             use core::internal::bounded_int;
             fn main() -> BoundedInt<0, 4294967294> {
-                let num = match bounded_int::trim::<u32, 0xffffffff>(0xfffffffe) {
+                let num = match bounded_int::trim_max::<u32>(0xfffffffe) {
                     OptionRev::Some(n) => n,
                     OptionRev::None => 0,
                 };
@@ -377,7 +377,7 @@ mod tests {
             use core::internal::{OptionRev, bounded_int::BoundedInt};
             use core::internal::bounded_int;
             fn main() -> BoundedInt<-32767, 32767> {
-                let num = match bounded_int::trim::<i16, -0x8000>(-0x8000) {
+                let num = match bounded_int::trim_min::<i16>(-0x8000) {
                     OptionRev::Some(n) => n,
                     OptionRev::None => 0,
                 };
